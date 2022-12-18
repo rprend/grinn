37fecad52da7d5113b1bc5787e9a65863a3850dc
Ryan Prendergast <rprendergast1121@gmail.com>
2022-12-18 10:48:03 -0500

    Add tailwind plugin

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import Head from 'next/head'
 import { ChangeEvent, useState } from 'react'
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Home() {
   const [joke, setJoke] = useState<string | null>(null)
   const [fetching, setFetching] = useState(false)
   let prompt = ''
+
+  const { data: session } = useSession()
 
   function updatePrompt(e: ChangeEvent) {
     prompt = (e.target as HTMLTextAreaElement).value
@@ -42,8 +45,15 @@ export default function Home() {
           <h1 className="pt-16 text-4xl font-bold">Grinn</h1>
           <h3 className="text-lg font-medium italic pb-6">the ai comic</h3>
           <div><textarea onChange={updatePrompt} className="p-5 w-full h-96 border-2 border-blue" /></div>
-          <button onClick={handleSubmit} className='text-white mt-8 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
-          >submit</button>
+          {!session &&
+          <button onClick={handleSubmit} data-tooltip-target="tooltip-default" className='text-white disabled:opacity-25 mt-8 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
+          disabled>submit</button>
+          }
+          <div id="tooltip-default" role="tooltip" className="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
+            Tooltip content
+            <div className="tooltip-arrow" data-popper-arrow></div>
+          </div>
+
           {joke && <div>{joke}</div>}
         </div>
       </main>
