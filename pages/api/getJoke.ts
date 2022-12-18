4febbec7c53b8e165694c1f1bfcefa769c4cdd6c
Ryan Prendergast <rprendergast1121@gmail.com>
2022-12-18 10:31:22 -0500

    Use the input prompt

--- pages/api/getJoke.ts
+++ pages/api/getJoke.ts
@@ -1,18 +1,21 @@
 
-import type { NextApiResponse } from "next/types";
+import type { NextApiRequest, NextApiResponse } from "next/types";
 import { Configuration, OpenAIApi } from "openai";
 
-export default async function handler(req, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
-
+  const prompt = req.body.prompt;
+  console.log(req.body)
+  console.log(prompt)
   const joke = await openai.createCompletion({
     model: "text-davinci-002",
-    prompt: "Gives me something here please, a story:",
+    prompt,
   });
 
+  console.log(joke)
   console.log(joke.data.choices[0].text);
   res.status(200).json( joke.data );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,28 @@
 import Head from 'next/head'
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 
 export default function Home() {
   const [joke, setJoke] = useState<string | null>(null)
   const [fetching, setFetching] = useState(false)
+  let prompt = ''
+
+  function updatePrompt(e: ChangeEvent) {
+    prompt = (e.target as HTMLTextAreaElement).value
+  }
 
   function handleSubmit() {
     // dont want to nuke my openai credits
     if (fetching) return
     setFetching(true)
-    fetch('api/getJoke').then(res => res.json()).then(data => {
+    fetch('api/getJoke', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        prompt
+      })
+    }).then(res => res.json()).then(data => {
       console.log(data)
       setJoke(data.choices[0].text)
       setFetching(false)
@@ -28,7 +41,7 @@ export default function Home() {
         <div className="w-3/4 md:w-2/3 lg:w-1/2 text-lg pb-20">
           <h1 className="pt-16 text-4xl font-bold">Grinn</h1>
           <h3 className="text-lg font-medium italic pb-6">the ai comic</h3>
-          <div><textarea className="p-5 w-full h-96 border-2 border-blue" /></div>
+          <div><textarea onChange={updatePrompt} className="p-5 w-full h-96 border-2 border-blue" /></div>
           <button onClick={handleSubmit} className='text-white mt-8 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
           >submit</button>
           {joke && <div>{joke}</div>}
